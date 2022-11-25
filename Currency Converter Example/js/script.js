7f85e8b9430c7ddcfb8e4b3456d7b5d0c6fd8054
Usman-Kashmiri <usmankashmiri378@gmail.com>
2022-11-25 11:17:32 +0500

    Added result container

--- Currency Converter Example/js/script.js
+++ Currency Converter Example/js/script.js
@@ -1,20 +1,28 @@
 // variables
 
 var select = document.querySelectorAll(".currency"),
+    calculationContainer = document.getElementById("calculationContainer"),
+    resultContainer = document.getElementById("resultContainer"),
     default_currency = document.querySelectorAll("#default_currency"),
     input_currency = document.getElementById('input_currency'),
     output_currency = document.getElementById('output_currency'),
     conversionAmount = document.getElementById('conversionAmount'),
     exchangeRate = document.getElementById('exchangeRate'),
-    timeStamp = document.getElementById('timeStamp'),
+    calculationTimeSpan = document.getElementById('timeStamp'),
     updateTime = document.getElementById('updateTime');
 
+
+calculationContainer.style.display = "block";
+resultContainer.style.display = "none";
+
 // fetching Gbp api
+
+var entries;
 
 fetch('http://www.floatrates.com/daily/gbp.json')
     .then((data) => data.json())
     .then((data) => {
-        const entries = Object.entries(data);
+        entries = Object.entries(data);
         console.log(entries)
         for (var i = 0; i < entries.length; i++) {
             select[0].innerHTML += `<option value="${i}">${entries[i][1].name} / ${entries[i][1].code}</option>`;
@@ -48,32 +56,64 @@ function roundOff(value) {
 
 function convert() {
     var calculation;
+    var timeDateNow = new Date();
+    yearNow = timeDateNow.getFullYear();
+    monthNow = timeDateNow.getMonth();
+    dateNow = timeDateNow.getDate();
+    hoursNow = timeDateNow.getHours();
+    minutesNow = timeDateNow.getMinutes();
+    var calculationTime = new Date(Date.UTC(yearNow, monthNow, dateNow, hoursNow, minutesNow));
+    const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric"
+      };
+    // var calculationTime = new timeStamp;
     var value1 = select[0].value;
     var value2 = select[1].value;
-    fetch('http://www.floatrates.com/daily/gbp.json')
-        .then((data) => data.json())
-        .then((data) => {
-            const entries = Object.entries(data);
-            input_currency_val = input_currency.value;
-            fromCurrency = entries[value1];
-            toCurrency = entries[value2];
-            if (input_currency.value <= 0) {
-                alert("Amount cannot be negative or zero!")
-            } else if (input_currency.value > 1000000000) {
-                alert("Conversion amount cannot be great than 1Billon!")
-            } else if (value1 == value2) {
-                alert("Please select two different currencies")
-            } else if (value1 == "149") {
-                calculation = toCurrency[1].rate * input_currency_val;
-            }
-            else if (select[1].value == "149") {
-                calculation = fromCurrency[1].inverseRate * input_currency_val;
-            } else {
-                calculation = toCurrency[1].rate / fromCurrency[1].rate * input_currency_val;
-            }
-            // output_currency.value = roundOff(calculation);
-            conversionAmount.innerHTML = roundOff(calculation);
-            exchangeRate.innerHTML = `1 ${fromCurrency[1].code} = ${toCurrency[1].rate} ${toCurrency[1].code}`;
-            window.location = 'result.html';
-        });
+    input_currency_val = input_currency.value;
+    var fromCurrency = entries[value1];
+    var toCurrency = entries[value2];
+    var fromCurrencyCode,
+    toCurrencyCode = toCurrency[1].code,
+    exchangeRateValue = toCurrency[1].rate,
+    RateUpdateTime = toCurrency[1].date;
+    if (input_currency.value <= 0) {
+        alert("Amount cannot be negative or zero!")
+    } else if (input_currency.value > 1000000000) {
+        alert("Conversion amount cannot be great than 1Billon!")
+    } else if (value1 == value2) {
+        alert("Please select two different currencies")
+    } else if (value1 == "149") {
+        calculation = toCurrency[1].rate * input_currency_val;
+        fromCurrencyCode = "GBP";
+        toCurrencyCode = toCurrency[1].code;
+        exchangeRateValue = toCurrency[1].rate;
+        // RateUpdateTime = toCurrency[1].date;
+    }
+    else if (value2 == "149") {
+        calculation = fromCurrency[1].inverseRate * input_currency_val;
+        fromCurrencyCode = fromCurrency[1].code;
+        toCurrencyCode = "GBP";
+        exchangeRateValue = toCurrency[1].innverseRate;
+        // RateUpdateTime = toCurrency[1].date;
+    } else {
+        calculation = toCurrency[1].rate / fromCurrency[1].rate * input_currency_val;
+        fromCurrencyCode = fromCurrency[1].code;
+    }
+    calculationContainer.style.display = "none";
+    resultContainer.style.display = "block";
+    conversionAmount.innerText = `${input_currency_val} ${fromCurrencyCode} = ${roundOff(calculation)} ${toCurrencyCode}`;
+    exchangeRate.innerText = `1 ${fromCurrencyCode} = ${roundOff(exchangeRateValue)} ${toCurrencyCode}`;
+    calculationTimeSpan.innerText = `${calculationTime.toLocaleString("en-GB", options)}`
+    updateTime.innerText = `${RateUpdateTime}`;
+    console.log()
+    // window.location = 'result.html';
+}
+
+function goBack() {
+    calculationContainer.style.display = "block";
+    resultContainer.style.display = "none";
 }
